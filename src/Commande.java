7b6ccc038299f6ea8ecdba4dd9fa1777051fa8c6
Sh3tys <titouanparzival@gmail.com>
2025-03-28 09:32:19 +0100

    New JavaClass for create a liste for a lot of plat and the total of this list

--- src/Commande.java
+++ src/Commande.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class Commande {
+    List<Plat> listPlats = new ArrayList<>();
+    double total;
+
+    Commande(){};
+    Commande(Plat plat){
+        listPlats.add(plat);
+        total = plat.prix;
+    }
+
+    public void afficher(){
+        System.out.println("La commande: ");
+        for (Plat plat : listPlats) {
+            System.out.println(plat);
+        }
+        System.out.println("Le total (en euros): " + total);
+    }
 }
